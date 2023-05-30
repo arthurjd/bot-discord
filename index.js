a5587ea79a7e4b5f2366654da59f07622af60ea2
Arthur <arthurjacob.domingues@gmail.com>
2023-05-30 00:46:15 -0300

    Base para comandos concluido

--- index.js
+++ index.js
@@ -1,44 +1,49 @@
-// Require the necessary discord.js classes
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+
+const { Client, Events, GatewayIntentBits, Collection, ReactionUserManager } = require('discord.js');
 
 const dotenv = require('dotenv')
 dotenv.config()
-const { TOKEN, CLIENT_ID, GUILD_ID } = process.env
+const { TOKEN, CLIENT_ID, GUILD_ID} = process.env
+
+//importação comandos
+const fs = require('node:fs')
+const path = require('node:path')
+
+const commandsPath = path.join(__dirname, "commands")
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'))
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+client.commands = new Collection()
+
+for (const file of commandFiles){
+	const filePath = path.join(commandsPath, file)
+	const command = require(filePath)
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command)
+	} else {
+		console.log(`Esse comando em ${filePath} esta com "data" ou "execute" ausentes`);
+	}
+}
 
 client.once(Events.ClientReady, c => {
 	console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
-client.login('TOKEN');
+client.login(TOKEN);
 
 
-
-/*
-  // Função que verifica se o usuário está jogando
-  async function checkActivity(userId, channelId, textChannelId) {
-    const guild = client.guilds.cache.first(); // Substitua isso para buscar a guilda correta se seu bot estiver em várias guildas
-    const member = await guild.members.fetch(userId);
-    const voiceChannel = guild.channels.cache.get(channelId);
-
-    if (member.voice.channel && member.voice.channel.id === voiceChannel.id) {
-      member.presence.activities.forEach((activity) => {
-        if (activity.type === 'PLAYING') {
-          const textChannel = guild.channels.cache.get(textChannelId);
-          textChannel.send(`${member.user.tag} está jogando ${activity.name}`);
-        }
-      });
-    }
-  }
-
-  // Substitua esses IDs pelos IDs corretos
-  const userId = '1112873336805015593';
-  const voiceChannelId = '1112873779270525012';
-  const textChannelId = '1112873779270525011';
-
-  // Verificar a atividade do usuário a cada minuto (60000 milissegundos)
-  setInterval(() => checkActivity(userId, voiceChannelId, textChannelId), 60000);
-
-  client.login('TOKEN'); // Substitua 'SEU_TOKEN' pelo token do seu bot
-*/
+client.on(Events.InteractionCreate, async interaction =>{
+	if (!interaction.isChatInputCommand()) return
+	const command = interaction.client.commands.get(interaction.commandName)
+	if (!command) {
+		console.error("Comando não encontrado")
+		return
+	}
+	try {
+		await command.execute(interaction)
+	}
+	catch (error) {
+		console.error(error)
+		await interaction.reply("Houve um erro ao executar esse comando")
+	}
+})
